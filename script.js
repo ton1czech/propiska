0e2d2a9734fd5583aebb52aafe9c2b980694da54
ton1czech <tonyasek007@gmail.com>
2022-05-04 19:12:07 +0200

    👌IMPROVE: anims

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.querySelector('.year').innerHTML = new Date().getFullYear()
 
-var tl = gsap.timeline({ defaults: { duration: 1.5 } })
-tl.from('#intxt1', { x: -500, opacity: 0 })
+var tl = gsap.timeline({ defaults: { duration: 1.3 } })
+tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 3 }, '+=1')
+  .from('#navbar', { y: 100, opacity: 0 }, '-=1.5')
+  .from('#intxt1', { x: -500, opacity: 0 })
   .from('#intxt2', { x: -500, opacity: 0 }, '-=1')
   .from('#intxt3', { x: -500, opacity: 0 }, '-=1')
   .from('#intxt4', { x: 500, opacity: 0 }, '-=0.3')
