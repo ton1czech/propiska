7059f2ffca558d45f28923d2f335eea592c2c929
ton1czech <tonyasek007@gmail.com>
2022-05-04 22:29:16 +0200

    👌IMPROVE: more changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('.year').innerHTML = new Date().getFullYear()
 
 var tl = gsap.timeline({ defaults: { duration: 1.3 } })
-tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 3 }, '+=1')
-  .from('#navbar', { y: 100, opacity: 0 }, '-=1.5')
-  .from('#intxt1', { x: -300, opacity: 0 })
+tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2.5 }, '+=1')
+  .from('#intxt1', { x: -300, opacity: 0 }, '-=1.2')
   .from('#intxt2', { x: -300, opacity: 0 }, '-=1')
   .from('#intxt3', { x: -300, opacity: 0 }, '-=1')
   .from('#intxt4', { x: 300, opacity: 0 }, '-=0.3')
+  .from('#navbar', { y: 100, opacity: 0 })
   .from('#intxt5', { y: 50, opacity: 0 }, '-=0.3')
