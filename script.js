488364f72bef4f7b112f2ea56b6c03f7745dad0b
ton1czech <tonyasek007@gmail.com>
2022-05-04 23:02:36 +0200

    👌IMPROVE: messing around with animations

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.querySelector('.year').innerHTML = new Date().getFullYear()
 
 // gsap
-var tl = gsap.timeline({ defaults: { duration: 1.3 } })
-tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2.5 }, '+=1')
+var tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2 }, '+=1')
   .from('#intxt1', { x: -300, opacity: 0 }, '-=1.2')
   .from('#intxt2', { x: -300, opacity: 0 }, '-=1')
-  .from('#intxt3', { x: -300, opacity: 0 }, '-=1')
+  .from('#intxt3', { x: -300, opacity: 0 }, '-=0.8')
   .from('#intxt4', { x: 300, opacity: 0 }, '-=0.3')
   .from('#navbar', { y: 100, opacity: 0 })
   .from('#intxt5', { y: 50, opacity: 0 }, '-=0.3')
