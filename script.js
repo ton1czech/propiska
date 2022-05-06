1ebeecc81320ca6bd42c44b51d186fced2cc2af1
ton1czech <tonyasek007@gmail.com>
2022-05-06 20:50:32 +0200

    🎁NEW: footer - contact

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.querySelector('.year').innerHTML = new Date().getFullYear()
 
-// gsap
 var tl = gsap.timeline({ defaults: { duration: 1 } })
 tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2 }, '+=1')
   .from('#intxt1', { x: -300, opacity: 0 }, '-=1.2')
@@ -10,20 +9,20 @@ tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2 }, '+=1')
   .from('#navbar', { y: 100, opacity: 0 })
   .from('#intxt5', { y: 50, opacity: 0 }, '-=0.3')
 
-// scrollreveal
-ScrollReveal({ opacity: 0, distance: '25%', delay: 600, origin: 'left' })
+ScrollReveal({ opacity: 0, distance: '25%', delay: 750, origin: 'left' })
 
-ScrollReveal().reveal('#abt1', { origin: 'top', delay: 1000 })
+ScrollReveal().reveal('#abt1', { origin: 'top' })
 ScrollReveal().reveal('#abt2')
 ScrollReveal().reveal('#abt3')
 ScrollReveal().reveal('#abt4')
 ScrollReveal().reveal('#abt5')
 ScrollReveal().reveal('#abt6')
 
-ScrollReveal().reveal('#abt7', { origin: 'top', delay: 1000 })
+ScrollReveal().reveal('#abt7', { origin: 'top' })
 ScrollReveal().reveal('#abt8')
 
-ScrollReveal().reveal('#buy1', { origin: 'top', delay: 1000 })
+ScrollReveal().reveal('#buy1', { origin: 'top' })
 ScrollReveal().reveal('#buy2', { origin: 'bottom' })
 
-ScrollReveal().reveal('#ftr1', { origin: 'right', delay: 1000 })
+ScrollReveal().reveal('#ftr1', { origin: 'top' })
+ScrollReveal().reveal('#ftr2', { origin: 'bottom' })
