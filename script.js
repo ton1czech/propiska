944720e2ddbc33faac014415d0059dd50de7d4e6
ton1czech <tonyasek007@gmail.com>
2022-05-13 17:40:51 +0200

    👌 IMPROVE: about section

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.querySelector('.year').innerHTML = new Date().getFullYear()
 
 var tl = gsap.timeline({ defaults: { duration: 1 } })
 tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2 }, '+=0.75')
-  .from('#intxt1', { x: -300, opacity: 0 }, '-=1.2')
-  .from('#intxt2', { x: -300, opacity: 0 }, '-=1')
-  .from('#intxt3', { x: -300, opacity: 0 }, '-=0.8')
+  .from('#int1', { x: -300, opacity: 0 }, '-=1.2')
+  .from('#int2', { x: -300, opacity: 0 }, '-=1')
+  .from('#int3', { x: -300, opacity: 0 }, '-=0.8')
   .fromTo(
-    '#pen',
+    '#int4',
     {
       clipPath: 'polygon(0 0, 0 0, 0 100%, 0 100%)',
     },
@@ -18,7 +18,7 @@ tl.from('#logo', { scale: 10, x: 500, y: 400, duration: 2 }, '+=0.75')
     '-=0.7'
   )
   .from('#navbar', { y: 100, opacity: 0 })
-  .from('#intxt5', { y: 50, opacity: 0 }, '-=0.3')
+  .from('#int5', { y: 50, opacity: 0 }, '-=0.3')
 
 ScrollReveal({ opacity: 0, distance: '25%', delay: 750, origin: 'left' })
 
@@ -26,11 +26,8 @@ ScrollReveal().reveal('#abt1', { origin: 'top' })
 ScrollReveal().reveal('#abt2')
 ScrollReveal().reveal('#abt3')
 ScrollReveal().reveal('#abt4')
-ScrollReveal().reveal('#abt5')
+ScrollReveal().reveal('#abt5', { origin: 'top' })
 ScrollReveal().reveal('#abt6')
-
-ScrollReveal().reveal('#abt7', { origin: 'top' })
-ScrollReveal().reveal('#abt8')
 
 ScrollReveal().reveal('#buy1', { origin: 'top' })
 ScrollReveal().reveal('#buy2', { origin: 'bottom' })
